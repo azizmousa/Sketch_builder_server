c1f61ddadb3ad5e182aba58f548396c67de8b259
Aziz Mousa <azizmousa1010@gmail.com>
2019-04-19 15:35:54 +0200

    System Configurations

---
+++ include/server/SystemConfiguration.h
@@ -0,0 +1,15 @@
+#ifndef SYSTEM_CONFIGURATION_
+#define SYSTEM_CONFIGURATION_
+
+#include <string>
+
+struct SystemConfiguration
+{
+    static std::string SKETCH_BUILDER_UI_PATH;
+    static std::string SKETCH_BUILDER_COMPILER_PATH;
+    static std::string SKETCH_BUILDER_EQUALIZER_PAHT;
+    static std::string SKETCH_BUILDER_GENERATOR_PATH;
+};
+
+
+#endif
