c24127f501cf25a94285ba956e18da1f4b0a6606
Aziz Mousa <azizmousa1010@gmail.com>
2019-04-19 16:35:53 +0200

    set ui path to start command

--- src/StartSystemCommand.cpp
+++ src/StartSystemCommand.cpp
@@ -1,9 +1,13 @@
 
 #include <bits/stdc++.h>
 #include <iostream>
+#include <string>
+#include "server/SystemConfiguration.h"
 #include "server/StartSystemCommand.h"
 
 void StartSystemCommand::doCommand(){
     // system("./system");
-    std::cout << "start command run" << std::endl;
+    std::string startCommand = "java -jar " + SystemConfiguration::getSystemUIPath();
+    system(startCommand.c_str());
+    std::cout << "start command run " << SystemConfiguration::getSystemUIPath()<< std::endl;
 }
