791f501a7a8bfc5c23590cdcbc78e2840f86012a
Aziz Mousa <azizmousa1010@gmail.com>
2019-04-19 20:38:30 +0200

    run start system command on thread

--- src/StartSystemCommand.cpp
+++ src/StartSystemCommand.cpp
@@ -2,12 +2,21 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <string>
+#include <thread>
+
 #include "server/SystemConfiguration.h"
 #include "server/StartSystemCommand.h"
 
+void handlCommand(std::thread * th){
+    std::string startCommand = "java -jar " + SystemConfiguration::getSystemUIPath();
+    int res = system(startCommand.c_str());
+    std::cout << "runing " << SystemConfiguration::getSystemUIPath()<< " result = "<<res<< std::endl;
+    delete th;
+}
+
 void StartSystemCommand::doCommand(){
     // system("./system");
-    std::string startCommand = "java -jar " + SystemConfiguration::getSystemUIPath();
-    system(startCommand.c_str());
-    std::cout << "start command run " << SystemConfiguration::getSystemUIPath()<< std::endl;
+    std::thread *commandThread = new std::thread;
+    *commandThread = std::thread(handlCommand, commandThread);
+    
 }
